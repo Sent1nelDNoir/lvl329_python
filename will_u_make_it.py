5c48d348cc4df8ea744eb8a996fb5d8dc29107b3
Sent1nelDNoir <40640252+Sent1nelDNoir@users.noreply.github.com>
2018-07-19 01:51:18 +0300

    Update will_u_make_it.py

--- will_u_make_it.py
+++ will_u_make_it.py
@@ -1,6 +1,6 @@
 # https://www.codewars.com/kata/will-you-make-it/train/python
 
 def zero_fuel(distance_to_pump, mpg, fuel_left):
-    if distance_to_pump<=(mpg * fuel_left):
-        return (True)  
-    return (False)  
+    if distance_to_pump <= mpg * fuel_left:
+        return True  
+    return False  

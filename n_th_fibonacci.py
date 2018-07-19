dda5fb79bab1c5b847d94b0967a2c5fd1b275f67
Sent1nelDNoir <40640252+Sent1nelDNoir@users.noreply.github.com>
2018-07-19 04:48:58 +0300

    Create n_th_fibonacci.py

---
+++ n_th_fibonacci.py
@@ -0,0 +1,14 @@
+#https://www.codewars.com/kata/n-th-fibonacci/train/python
+
+def nth_fib(n):
+    f1, f2 = 0, 1
+    i = 2
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        while i < n:
+            f1, f2 = f2, f1 + f2
+            i += 1
+        return f2

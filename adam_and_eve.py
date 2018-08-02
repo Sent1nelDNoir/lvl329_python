52188d45b4fc087eb080e9500e086c34810ce872
Sent1nelDNoir <40640252+Sent1nelDNoir@users.noreply.github.com>
2018-08-02 15:39:39 +0300

    Create  adam_and_eve.py

---
+++ adam_and_eve.py
@@ -0,0 +1,11 @@
+https://www.codewars.com/kata/547274e24481cfc469000416/solutions/python
+
+class Human:
+    pass
+class Man(Human):
+    pass
+class Woman(Human):
+    pass
+    
+def God():
+    return Man(),Woman()
